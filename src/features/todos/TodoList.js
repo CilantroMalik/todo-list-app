b7a66eb5421526d8b8b5e44ed8ec10610ede7602
Rohan Malik <rohan_malik@ryecountryday.org>
2021-05-22 22:37:03 -0400

    Documentation for TodoList file

--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -12,20 +12,25 @@ export const TodoList = () => {
 
     const dispatch = useDispatch()
 
+    // handler for creating a new task: trigger the animation and switch to the creating view
     const createTaskClicked = () => {
         dispatch(animateCreateTask(true))
         dispatch(toggleCreating(true))
     }
 
+    // handler for cancelling task creation: trigger the corresponding animation and remove the creating view
     const cancelClicked = () => {
         dispatch(animateCancelTask(true))
         dispatch(toggleCreating(false))
     }
 
+    // handler for completing a task: trigger the corresponding task ID's completion toggle
     const completeClicked = taskId => {
         dispatch(todoCompleted(taskId))
     }
 
+    // create the HTML elements for each to do item, creating a column layout with different styles depending on whether
+    // the task is completed or not; also have a button to complete the task or an indicator that it is complete
     const renderedTodos = todos.map(todoItem => (
         <div style={{breakInside: 'avoid-column'}}>
             <article className={todoItem.done ? styles.itemDone : styles.item} key={todoItem.id}>
@@ -37,6 +42,8 @@ export const TodoList = () => {
         </div>
     ))
 
+    // simply return a section that fades in on rendering, and that either shows the create or cancel button as well as
+    // the list of to do items in the columnar layout
     return (
         <section className={styles.fadeInElement}>
             { !creation.creation ? <button onClick={createTaskClicked}>Create Task</button> : <button onClick={cancelClicked}>Cancel</button>}
