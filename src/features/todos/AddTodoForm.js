104eeb75f4ba1bc88a938e56806a35e143cbd18d
Rohan Malik <rohan_malik@ryecountryday.org>
2021-05-24 20:21:12 -0400

    Comments for AddTodoForm component

--- src/features/todos/AddTodoForm.js
+++ src/features/todos/AddTodoForm.js
@@ -8,40 +8,42 @@ import '../../main.css'
 import {animateCreateTask, animateCancelTask} from "./animationSlice";
 
 export const AddTodoForm = () => {
-    const [title, setTitle] = useState('')
-    const [desc, setDesc] = useState('')
-    const [titleError, setTitleError] = useState(false)
-    const [descError, setDescError] = useState(false)
+    const [title, setTitle] = useState('')  // controls the state for the title field
+    const [desc, setDesc] = useState('')  // controls the state for the description field
+    const [titleError, setTitleError] = useState(false)  // stores whether the title field has an error (toggles a red-border CSS class)
+    const [descError, setDescError] = useState(false)  // stores whether the description field has an error
 
-    const animationAppear = useSelector(state => state.animation.createTaskAnimating)
-    const animationDisappear = useSelector(state => state.animation.cancelTaskAnimating)
+    const animationAppear = useSelector(state => state.animation.createTaskAnimating)  // stores the appear animation state
+    const animationDisappear = useSelector(state => state.animation.cancelTaskAnimating)  // stores the disappear animation state
 
     const dispatch = useDispatch()
 
-    const onTitleChanged = e => { setTitle(e.target.value); setTitleError(false) }
-    const onDescChanged = e => { setDesc(e.target.value); setDescError(false) }
+    const onTitleChanged = e => { setTitle(e.target.value); setTitleError(false) }  // update title and clear the error state
+    const onDescChanged = e => { setDesc(e.target.value); setDescError(false) }  // do the same for description
 
+    // handles a create task button click
     const onCreateTaskClicked = () => {
-        if (title && desc) {
-            dispatch(todoAdded({id: nanoid(), title, desc, done: false}))
-            dispatch(animateCancelTask(true))
-            setTimeout(() => dispatch(toggleCreating(false)), 375)
-            setTitle('')
+        if (title && desc) {  // checks if the title and description fields are non-empty
+            dispatch(todoAdded({id: nanoid(), title, desc, done: false}))  // add the to do item to our store
+            dispatch(animateCancelTask(true))  // and trigger the disappear animation
+            setTimeout(() => dispatch(toggleCreating(false)), 375)  // after the animation is done, hide the creating view
+            setTitle('')  // clear the title and description for the next time
             setDesc('')
-        } else {
+        } else {  // if one or more fields are empty, give them the error state
             if (!title) { setTitleError(true) }
             if (!desc) { setDescError(true) }
         }
     }
 
+    // handles a cancel task button click
     const onCancelClicked = () => {
-        dispatch(animateCancelTask(true))
-        setTimeout(() => dispatch(toggleCreating(false)), 375)
-        setTitle('')
+        dispatch(animateCancelTask(true))  // trigger the disappear animation
+        setTimeout(() => dispatch(toggleCreating(false)), 375)  // and hide the creating view after a delay
+        setTitle('')  // also clear the title and description fields
         setDesc('')
     }
 
-    return (
+    return (  // animate the section by adding an animator class if the corresponding state is true, and then set it to false when the animation is over
         <section className={animationAppear ? styles.animation_appearUp : (animationDisappear ? styles.animation_disappearDown : "")}
         onAnimationEnd={() => {if (animationAppear) {dispatch(animateCreateTask(false))} if (animationDisappear) {dispatch(animateCancelTask(false))}}}>
             <h2>Add New Task</h2>
